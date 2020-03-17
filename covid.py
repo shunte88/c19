6ae21852439eb1bd472edd8f0085c9e51c66d304
Stuart Hunter <stuart.hunter@comcast.net>
2020-03-17 15:05:25 -0400

    3.6

--- covid.py
+++ covid.py
@@ -10,7 +10,20 @@ def getCovidAttr(c19attr):
     return dataset[c19attr].sum()
 
 
+def getCovidAttr3(c19attr):
+    import requests
+    c19url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-{c19attr}.csv'
+    df={'names': ('Location', 'Country', c19attr),'formats': ('S128', 'S128', 'i')}
+    with requests.get(c19url, headers={'Accept': 'application/text'}) as url:
+        if 200 == url.status_code:
+            dataset = np.loadtxt(url.content.decode('utf-8').splitlines(), dtype=df, delimiter=",", skiprows=1, usecols=(0, 1, -1))
+            return dataset[c19attr].sum()
+    return -1
+
+
 for attr in ('Confirmed','Recovered','Deaths'):
     print(attr,getCovidAttr(attr))
+    print(attr,getCovidAttr3(attr))
+
 
 
